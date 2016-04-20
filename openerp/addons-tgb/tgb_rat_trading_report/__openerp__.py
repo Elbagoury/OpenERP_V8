a4f5fce24a7041132e482885e93ca6f2273481e7
Phung Pham <minhphung171093@gmail.com>
2016-04-20 15:57:53 +0700

    up

--- openerp/addons-tgb/tgb_rat_trading_report/__openerp__.py
+++ openerp/addons-tgb/tgb_rat_trading_report/__openerp__.py
@@ -6,7 +6,7 @@
     "author" : "Son Pham.",
     "category": 'TGB',
     'complexity': "easy",
-    'depends': ['sale','account','report_aeroo'],
+    'depends': ['sale','TGB_RatTrading','account','report_aeroo'],
     "description": """
         This module provides the functionality to store digital signature image for a record.
         The example can be seen into the User's form view where we have added a test field under signature.
