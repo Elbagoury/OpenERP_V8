064a65a44ef969d9756ad50cb0fec9c23dd37f0e
Phung Pham <minhphung171093@gmail.com>
2016-05-24 11:12:21 +0700

    update

--- openerp/addons-supersonic/tgb_supersonic_purchase/purchase.py
+++ openerp/addons-supersonic/tgb_supersonic_purchase/purchase.py
@@ -12,6 +12,16 @@ class purchase_order(osv.osv):
         'quotation_dated':fields.date('Dated'),
         'delivery':fields.char('Delivery', size=1024),
         'warranty':fields.char('Warranty', size=1024),
+        'area_re':fields.text('area RE'),
+        'terms':fields.char('Terms'),
+    }
+    
+    _defaults = {
+        'area_re': '''Reference to the above-mentioned project and your Quotation Ref : , dated: , we are pleased to confirm our order to you as follows:-''',
+        'terms': '30 days upon presentation of your Tax Invoice.',
+        'delivery': 'To follow strictly to our work schedule.',
+        'warranty': 'To provide 12 months warranty from the date of our successful testing and commissioning.',
+        'notes': '''Not withstanding any information and technical particulars submitted.  All materials / equipment offer shall comply fully to the standard code of practice, Consultants tender specification for this project. Any equipment not complied to specification shall be made fully compliance at your own cost.''',
     }
     
 purchase_order()
