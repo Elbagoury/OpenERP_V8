a5a4a49600220d5ab159576d8ee7182a19e802cb
Phung Pham <minhphung171093@gmail.com>
2016-07-11 20:22:15 +0700

    up

--- openerp/addons-music-lodge/tgb_music_lodge/__openerp__.py
+++ openerp/addons-music-lodge/tgb_music_lodge/__openerp__.py
@@ -24,7 +24,7 @@
     'version': '1.0',
     'category': 'TGB',
     'sequence': 1,
-    'depends': ['sale','account','report_aeroo'],
+    'depends': ['sale','account','account_accountant','account_voucher','report_aeroo'],
     'data': [
         'report/rental_agreement_report_view.xml',
         'report/invoice_report_view.xml',
